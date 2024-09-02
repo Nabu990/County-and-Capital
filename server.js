80949bef92e10c40d1daa62eb97d56f1bddbac7b
SIr kelvin <Nabuthouth@gmail.com>
2024-09-02 15:30:27 +0000

    secound commit

--- server.js
+++ server.js
@@ -1,23 +1,46 @@
 const express = require('express');
-const app = express(); // express app
-const port = 3000; // port
-const path = require('path');
-const countries = require('./module/function');
+const bodyParser = require('body-parser');
 
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'pubilc')));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended: true }));
+const counties = [
+    { county: "Bomi", capital: "Tubmanburg" },
+    { county: "Bong", capital: "Gbarnga" },
+    { county: "Gbarpolu", capital: "Bopolu" },
+    { county: "Grand Bassa", capital: "Buchanan" },
+    { county: "Grand Cape Mount", capital: "Robertsport" },
+    { county: "Grand Gedeh", capital: "Zwedru" },
+    { county: "Grand Kru", capital: "Barclayville" },
+    { county: "Lofa", capital: "Voinjama" },
+    { county: "Margibi", capital: "Kakata" },
+    { county: "Maryland", capital: "Harper" },
+    { county: "Montserrado", capital: "Bensonville" }, // Monrovia is the national capital but Bensonville is the county capital
+    { county: "Nimba", capital: "Sanniquellie" },
+    { county: "River Cess", capital: "Cestos City" },
+    { county: "River Gee", capital: "Fish Town" },
+    { county: "Sinoe", capital: "Greenville" }
+];
 
-app.get('/', function (req, res) {
-  const randomN = Math.floor(Math.random() * 15);
-  res.render('index.ejs', { countries: countries, index: randomN });
+app.get('/', (req, res) => {
+  const randomCounty = counties[Math.floor(Math.random() * counties.length)];
+  res.render('index', { county: randomCounty });
 });
 
-// app.post('/checkresult', (req, res) => {
-//   res.send(req.body);
-// });
+app.post('/submit', (req, res) => {
+  const userAnswer = req.body.answer.toLowerCase();
+  const countyName = req.body.county;
+  const county = counties.find(c => c.county === countyName);
+  const isCorrect = userAnswer === county.capital.toLowerCase();
+
+  res.render('result', { county: county.county, correctAnswer: county.capital, userAnswer, isCorrect });
+});
+
 
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+  console.log(`Quiz app listening at http://localhost:${port}`);
 });
